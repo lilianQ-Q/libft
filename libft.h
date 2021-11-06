0f6b6af6eb96ebdcd7b89c395dfa779c388b73a0
Lilian Damiens <ldamiens@z1r7p1.42lyon.fr>
2021-11-06 15:07:41 +0100

    add libft.h

---
+++ libft.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldamiens <ldamiens@student.42lyon.f>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 14:13:41 by ldamiens          #+#    #+#             */
+/*   Updated: 2021/11/06 13:25:44 by ldamiens         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stddef.h>
+
+void	ft_bzero(void *string, size_t n);
+int		ft_isalnum(int character);
+int		ft_isalpha(int character);
+int		ft_isascii(int character);
+int		ft_isdigit(int character);
+int		ft_isprint(int character);
+void	*ft_memchr(const void *ptr, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *ptr, int value, size_t count);
+char	*ft_strchr(const char *string, int character);
+size_t	ft_strlen(const char *string);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *string, int needle);
+int		ft_tolower(int character);
+int		ft_toupper(int character);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+
+#endif
