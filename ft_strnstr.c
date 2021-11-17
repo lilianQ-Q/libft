71d5bd2e31285bd03ccc5a247a6ee780c54e0062
Lilian Damiens <ldamiens@z1r9p2.42lyon.fr>
2021-11-17 11:18:59 +0100

    [IMP] libft: add one new function
    This commit adds one new function to the library:
     - ft_strnstr.c: Returns the first occurence of a string into another
     where not more that n character is searched

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,36 @@
 /*   By: ldamiens <ldamiens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 11:35:35 by ldamiens          #+#    #+#             */
-/*   Updated: 2021/11/11 17:30:15 by ldamiens         ###   ########.fr       */
+/*   Updated: 2021/11/13 14:31:48 by ldamiens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
-#include <libft.h>
+#include "libft.h"
+#include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	index;
-	size_t	second;
+	size_t	i;
+	size_t	needlelen;
 
-	index = 0;
-	if (!needle[0])
-		return (&((char *)haystack)[0]);
-	while (index < len && haystack[index])
+	i = 0;
+	needlelen = ft_strlen(needle);
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i + needlelen <= len)
 	{
-		if (haystack[index] == needle[0])
-		{
-			second = 0;
-			while (haystack[index + second] && needle[second])
-			{
-				if (needle[second + 1] == '\0')
-					if (ft_strlen(&((char *)haystack)[index]) <= len)
-						return (&((char *)haystack)[index]);
-				second++;
-			}
-			if (second == ft_strlen(needle) && second < len)
-				return (&((char *)haystack)[index]);
-		}
-		index++;
+		if (ft_strncmp(haystack + i, needle, needlelen) == 0)
+			return ((char *)haystack + i);
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
+/*  int main(void)
+{
+	char haystack[] = "aacababcd";
+	char needle[] = "cd";
+	printf("%s\n", ft_strnstr(haystack, needle, 8));
+	printf("%s\n", strnstr(haystack, needle, 8));
+} */
