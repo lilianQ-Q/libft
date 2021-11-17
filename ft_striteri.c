9dd7fbf758a6cf6f226ffca6d48a1409336cf949
Lilian Damiens <ldamiens@z1r9p2.42lyon.fr>
2021-11-17 11:27:45 +0100

    norminette

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ldamiens <ldamiens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:49:10 by ldamiens          #+#    #+#             */
-/*   Updated: 2021/11/16 18:53:45 by ldamiens         ###   ########.fr       */
+/*   Updated: 2021/11/17 11:27:29 by ldamiens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int	index;
 
-    if (!s)
-        return ;
+	if (!s)
+		return ;
 	index = 0;
 	while (s[index])
 	{
